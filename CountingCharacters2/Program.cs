5a0ee389c1a7833dc3c4b68a40fddbb46b4971ac
thinkrealhard <allen.touchstone@gmail.com>
2020-06-04 03:40:04 -1000

    Add project files.

---
+++ CountingCharacters2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountingCharacters2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus";
+
+            Dictionary<char, int> letters = new Dictionary<char, int>();
+
+            foreach (char character in lorem)
+            {
+                if (Char.IsLetter(character))
+                {
+                    if (letters.ContainsKey(character))
+                    {
+                        letters[character] += 1;
+                    }
+                    else
+                    {
+                        letters.Add(character, 1);
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<char, int> count in letters)
+            {
+                Console.WriteLine($"{count.Key} : {count.Value}");
+            }
+        }
+    }
+}
